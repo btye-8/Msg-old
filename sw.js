f36a402dddb5e06269e19704b3670c04c45a056e
btye-8 <trail.demo58@gmail.com>
2025-06-29 13:10:15 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,14 @@
 self.addEventListener('push', function (event) {
   const options = {
     body: 'You have a new message',
-    icon: 'icon.png',
-    badge: 'icon.png',
+    icon: 'v8.png',
+    badge: 'v8.png',
     data: { url: 'index.html' },
     requireInteraction: true
   };
 
   event.waitUntil(
-    self.registration.showNotification('Friend Messenger', options)
+    self.registration.showNotification('Messenger', options)
   );
 });
 
